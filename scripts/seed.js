e0798a52cc6b76cea1592164b0b377ec8e0c2e18
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-15 00:33:42 +0300

    some mode

--- scripts/seed.js
+++ scripts/seed.js
@@ -162,7 +162,6 @@ async function seedRevenue(client) {
 
 async function main() {
   const client = await db.connect();
-
   await seedUsers(client);
   await seedCustomers(client);
   await seedInvoices(client);
